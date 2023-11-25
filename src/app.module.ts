b2c126de8deed805253bf6df8b593e9b766ce0d6
Jore Parra <claudio_san_martin@hotmail.com>
2023-11-25 01:17:17 -0300

    Creacion Modulos RES Productos y Ventas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { VentasModule } from './ventas/ventas.module';
+import { ProductosModule } from './productos/productos.module';
 
 @Module({
 
   
-  imports: [MongooseModule.forRoot('mongodb://mongo:clave123@localhost:27017/TiendaMongo')],
+  imports: [MongooseModule.forRoot('mongodb://mongo:clave123@localhost:27017/TiendaMongo'), ProductosModule, VentasModule],
   controllers: [AppController],
   providers: [AppService],
 })
